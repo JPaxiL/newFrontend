a81fb0eaa6724684f21d92198d673ac86af75152
jonathan2424 <econde@gltracker.pe>
2021-09-30 17:17:05 -0500

    ng test vehicle

--- src/app/vehicles/services/vehicle.service.spec.ts
+++ src/app/vehicles/services/vehicle.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { VehicleService } from './vehicle.service';
 
@@ -6,7 +7,9 @@ describe('VehicleService', () => {
   let service: VehicleService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
     service = TestBed.inject(VehicleService);
   });
 
