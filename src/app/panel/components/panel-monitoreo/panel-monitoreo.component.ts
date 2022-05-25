5f0b3a4bcc2699f119a310e5b720d3cfb2500bdc
Edgardo Parra <ed.parraolarte@gmail.com>
2022-05-25 13:38:42 -0500

    Resizeable panelMonitoreo + Estilos en tabla panel vehículos

--- src/app/panel/components/panel-monitoreo/panel-monitoreo.component.ts
+++ src/app/panel/components/panel-monitoreo/panel-monitoreo.component.ts
@@ -12,7 +12,31 @@ export class PanelMonitoreoComponent implements OnInit {
 
   constructor( public panelService: PanelService ) { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+
+    /* Redimensionar el window resize */
+    window.addEventListener('resize', function(e: any) {
+      if(this.document.getElementById('panelMonitoreo')!.getBoundingClientRect().width < 0.25 * window.innerWidth) { 
+        $('#panelMonitoreo').css("width", 0.25 * window.innerWidth);
+      } else if (this.document.getElementById('panelMonitoreo')!.getBoundingClientRect().width > 0.75 * window.innerWidth) {
+        $('#panelMonitoreo').css("width", 0.75 * window.innerWidth);
+      }
+    }, true);
+
+    /* Redimiensionar en drag */
+    $('#dragbar').mousedown(function(e:any){
+      e.preventDefault();
+      $(document).mousemove(function(e:any){
+        $('#dragbar').addClass('dragging');
+        $('#panelMonitoreo').css("width",(e.pageX<0.25 * window.innerWidth? 0.25 * window.innerWidth + 2: (e.pageX> 0.75 * window.innerWidth? 0.75 * window.innerWidth + 2: e.pageX + 2)));
+     });
+    });
+    $(document).mouseup(function(e:any){
+      $('#dragbar').removeClass('dragging');
+      $(document).unbind('mousemove');
+    });
+
+  }
 
   clickHidePanel(): void {
     $("#panelMonitoreo").hide( "slow" );
