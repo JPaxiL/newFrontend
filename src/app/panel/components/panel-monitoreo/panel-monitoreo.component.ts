8eda3f6eded63875ffebd21fb6aefcc415be2c6e
Edgardo Parra <ed.parraolarte@gmail.com>
2022-06-06 14:28:01 -0500

    Panel Monitoreo resizeable con botón

--- src/app/panel/components/panel-monitoreo/panel-monitoreo.component.ts
+++ src/app/panel/components/panel-monitoreo/panel-monitoreo.component.ts
@@ -9,12 +9,13 @@ declare var $: any;
   styleUrls: ['./panel-monitoreo.component.scss']
 })
 export class PanelMonitoreoComponent implements OnInit {
+  
 
   constructor( public panelService: PanelService ) { }
 
   ngOnInit(): void {
-
-    /* Redimensionar el window resize */
+    const dragbarToggleWidth = 20; /* 16 margen del elemento + 4 */ 
+    /* Redimensionar panel en window resize */
     window.addEventListener('resize', function(e: any) {
       if(this.document.getElementById('panelMonitoreo')!.getBoundingClientRect().width < 0.275 * window.innerWidth) { 
         $('#panelMonitoreo').css("width", 0.275 * window.innerWidth);
@@ -23,15 +24,15 @@ export class PanelMonitoreoComponent implements OnInit {
       }
     }, true);
 
-    /* Redimiensionar en drag */
-    $('#dragbar').on('mousedown touchstart gesturestart', function(e:any){
+    /* Redimiensionar panel en drag */
+    $('#dragbar > div').on('mousedown touchstart gesturestart', function(e:any){
       $('#dragbar').addClass('dragging');
       e.preventDefault();  
       $(document).on('mousemove touchmove gesturechange', function(e:any){
         if(e.originalEvent.touches){
-          $('#panelMonitoreo').css("width",(e.targetTouches[0].pageX<0.275 * window.innerWidth? 0.275 * window.innerWidth + 2: (e.targetTouches[0].pageX> 0.725 * window.innerWidth? 0.725 * window.innerWidth + 2: e.targetTouches[0].pageX + 2)));
+          $('#panelMonitoreo').css("width",(e.targetTouches[0].pageX - dragbarToggleWidth < 0.275 * window.innerWidth? 0.275 * window.innerWidth - dragbarToggleWidth: (e.targetTouches[0].pageX - dragbarToggleWidth > 0.725 * window.innerWidth? 0.725 * window.innerWidth - dragbarToggleWidth: e.targetTouches[0].pageX - dragbarToggleWidth)));
         } else {
-          $('#panelMonitoreo').css("width",(e.pageX<0.275 * window.innerWidth? 0.275 * window.innerWidth + 2: (e.pageX> 0.725 * window.innerWidth? 0.725 * window.innerWidth + 2: e.pageX + 2)));
+          $('#panelMonitoreo').css("width",(e.pageX - dragbarToggleWidth < 0.275 * window.innerWidth? 0.275 * window.innerWidth - dragbarToggleWidth: (e.pageX - dragbarToggleWidth > 0.725 * window.innerWidth? 0.725 * window.innerWidth - dragbarToggleWidth: e.pageX - dragbarToggleWidth)));
         }
      });
     });
