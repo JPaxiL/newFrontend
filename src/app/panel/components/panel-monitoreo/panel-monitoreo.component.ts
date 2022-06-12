e0eaa95156338ff73ca9924ba770a7ebe3782640
Edgardo Parra <ed.parraolarte@gmail.com>
2022-06-11 23:47:49 -0500

    Estilos Panel Vehiculos + Vista general autoWidth + Quitado GPS y GSM + mejora de precision de draggable

--- src/app/panel/components/panel-monitoreo/panel-monitoreo.component.ts
+++ src/app/panel/components/panel-monitoreo/panel-monitoreo.component.ts
@@ -14,25 +14,45 @@ export class PanelMonitoreoComponent implements OnInit {
   constructor( public panelService: PanelService ) { }
 
   ngOnInit(): void {
-    const dragbarToggleWidth = 20; /* 16 margen del elemento + 4 */ 
+    var clickDistanceFromDragbar: number;
+    var maxWidth: number;
+    var minWidth: number;
+    
     /* Redimensionar panel en window resize */
     window.addEventListener('resize', function(e: any) {
-      if(this.document.getElementById('panelMonitoreo')!.getBoundingClientRect().width < 0.275 * window.innerWidth) { 
-        $('#panelMonitoreo').css("width", 0.275 * window.innerWidth);
-      } else if (this.document.getElementById('panelMonitoreo')!.getBoundingClientRect().width > 0.725 * window.innerWidth) {
-        $('#panelMonitoreo').css("width", 0.725 * window.innerWidth);
+      minWidth = 425;
+      if(window.innerWidth > 600) {
+        maxWidth = 0.725 * window.innerWidth < 800? 0.725 * window.innerWidth: 800;
+      } else {
+        maxWidth = minWidth;
+      }
+
+      if(this.document.getElementById('panelMonitoreo')!.getBoundingClientRect().width < minWidth) {
+        $('#panelMonitoreo').css("width", minWidth);
+      } 
+      else if(this.document.getElementById('panelMonitoreo')!.getBoundingClientRect().width > maxWidth) { 
+        $('#panelMonitoreo').css("width", maxWidth);
       }
     }, true);
 
     /* Redimiensionar panel en drag */
     $('#dragbar > div').on('mousedown touchstart gesturestart', function(e:any){
+      minWidth = 425;
+      if(window.innerWidth > 600) {
+        maxWidth = 0.725 * window.innerWidth < 800? 0.725 * window.innerWidth: 800;
+      } else {
+        maxWidth = minWidth;
+      }
+
+      clickDistanceFromDragbar = e.originalEvent.touches? e.targetTouches[0].pageX - document.getElementById('panelMonitoreo')!.offsetWidth: e.pageX - document.getElementById('panelMonitoreo')!.offsetWidth;
+
       $('#dragbar').addClass('dragging');
       e.preventDefault();  
       $(document).on('mousemove touchmove gesturechange', function(e:any){
         if(e.originalEvent.touches){
-          $('#panelMonitoreo').css("width",(e.targetTouches[0].pageX - dragbarToggleWidth < 0.275 * window.innerWidth? 0.275 * window.innerWidth - dragbarToggleWidth: (e.targetTouches[0].pageX - dragbarToggleWidth > 0.725 * window.innerWidth? 0.725 * window.innerWidth - dragbarToggleWidth: e.targetTouches[0].pageX - dragbarToggleWidth)));
+          $('#panelMonitoreo').css("width",(e.targetTouches[0].pageX - clickDistanceFromDragbar < minWidth? minWidth: (e.targetTouches[0].pageX - clickDistanceFromDragbar > maxWidth? maxWidth: e.targetTouches[0].pageX - clickDistanceFromDragbar)));
         } else {
-          $('#panelMonitoreo').css("width",(e.pageX - dragbarToggleWidth < 0.275 * window.innerWidth? 0.275 * window.innerWidth - dragbarToggleWidth: (e.pageX - dragbarToggleWidth > 0.725 * window.innerWidth? 0.725 * window.innerWidth - dragbarToggleWidth: e.pageX - dragbarToggleWidth)));
+          $('#panelMonitoreo').css("width",(e.pageX - clickDistanceFromDragbar < minWidth? minWidth: (e.pageX - clickDistanceFromDragbar > maxWidth? maxWidth: e.pageX - clickDistanceFromDragbar)));
         }
      });
     });
