e5d4b0b88e4e80af56148a7eb987f9ee3e377aad
j_jimenezm <jjimenez@gltracker.pe>
2023-08-24 16:10:38 -0500

    update apiUrl

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  apiUrl: 'https://apiprueba.glmonitoreo.com',
+  production: true,
+  apiUrl: 'https://apiplataforma.glmonitoreo.com',
   idClient: '106',
   secretClient: 'au2vdKGrRK013Sy1rlgu5H7yb9k1LOzTx5wXz9Uu',
   socketUrl: 'https://escucha.glmonitoreo.com/'
