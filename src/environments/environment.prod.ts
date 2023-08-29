f8ef7a2cddac19f152ca3907541722678165f77e
j_jimenezm <jjimenez@gltracker.pe>
2023-08-29 12:04:14 -0500

    deploy development

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://apiprueba.glmonitoreo.com',
+  apiUrl: 'https://apiplataforma.glmonitoreo.com',
   idClient: '106',
   secretClient: 'au2vdKGrRK013Sy1rlgu5H7yb9k1LOzTx5wXz9Uu',
   socketUrl: 'https://escucha.glmonitoreo.com/'
