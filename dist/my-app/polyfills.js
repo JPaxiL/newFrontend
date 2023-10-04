ecdfe507c648ac2529c36fd0b848525c5c7ac07b
aencinas0208 <aencinas@gltracker.pe>
2023-10-04 11:00:17 -0500

    update images default icons svg for vehicles

--- dist/my-app/polyfills.js
+++ dist/my-app/polyfills.js
@@ -7,7 +7,7 @@
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /Users/ti/Documents/GLTracker/NewPlatform/newFrontend/src/polyfills.ts */"hN/g");
+module.exports = __webpack_require__(/*! /Users/jjimenez/Documents/GLTracker/NUEVA PLATAFORMA/newFrontend/src/polyfills.ts */"hN/g");
 
 
 /***/ }),
