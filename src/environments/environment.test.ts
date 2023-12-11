4c06452d5b2d6b7ee54cf48b7170c8beae9a2766
j_jimenezm <jjimenez@gltracker.pe>
2023-12-11 08:50:08 -0500

    add

--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -3,7 +3,7 @@ export const environment = {
   // apiUrl: 'https://apiprueba.glmonitoreo.com',
   // idClient: '84',
   // secretClient: 'LUTj1k8Tley9SvQ1Jwmrha1HPHJs87ShfiQVB18S',
-  apiUrl: 'https://apidev.glmonitoreo.com',
+  apiUrl: 'https://apitest.glmonitoreo.com',
   idClient: '106',
   secretClient: 'au2vdKGrRK013Sy1rlgu5H7yb9k1LOzTx5wXz9Uu',
   socketUrl: 'https://escucha.glmonitoreo.com/',
