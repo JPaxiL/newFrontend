6c431f84cd6101c42d17eb1e6858ddd0c78d67b9
j_jimenezm <jjimenez@gltracker.pe>
2023-12-27 09:47:25 -0500

    fix alerts

--- src/app/alerts/service/alert.service.ts
+++ src/app/alerts/service/alert.service.ts
@@ -30,8 +30,7 @@ export class AlertService {
     { id: 6, ruta: 'sonidos/Ping6.mp3', label: 'Sonido 6' },
     { id: 7, ruta: 'sonidos/Twitter7.mp3', label: 'Sonido 7' },
     { id: 8, ruta: 'sonidos/Whatsap8.mp3', label: 'Sonido 8' },
-    { id: 9, ruta: 'sonidos/WhatsappSound9.mp3', label: 'Sonido 9' },
-    { id: 10, ruta: '', label: 'Sin Sonido' },
+    { id: 9, ruta: 'sonidos/WhatsappSound9.mp3', label: 'Sonido 9' }
   ];
 
   constructor(private http: HttpClient) { 
