06579e41f93e7ed72264dbcf8c459bcbe2db8317
JohanStudios <159792752+JohanStudios@users.noreply.github.com>
2024-02-21 17:33:32 -0500

    import ver 1.5

--- src/app/geofences/geofences-modal/geofences-modal.component.ts
+++ src/app/geofences/geofences-modal/geofences-modal.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { GeofencesService } from '../services/geofences.service';
-import { FormGroup } from '@angular/forms';
-import Swal from 'sweetalert2';
+
 
 @Component({
   selector: 'app-geofences-modal',
@@ -19,20 +18,20 @@ export class GeofencesModalComponent implements OnInit {
   public velZona: string;
   public tiempoActZona: string;
   public geom: string;
-  public catZona: number=0;
-  public vel2Zona: number=0;
-  public vel3Zona:number=0;
-  public velMax: number=0;
+  public catZona: number = 0;
+  public vel2Zona: number = 0;
+  public vel3Zona: number = 0;
+  public velMax: number = 0;
   public updatedAt: Date;
   public descripcion: string;
   public bolEliminado: boolean;
-  public tagNameFontSize: number=0;
+  public tagNameFontSize: number = 0;
   public zoneType: string;
-  public grupoConvoyId: number=0;
-  public operationGrupeId: number=0;
+  public grupoConvoyId: number = 0;
+  public operationGrupeId: number = 0;
   public geoTags: string;
 
-  constructor(public geofencesService: GeofencesService) { 
+  constructor(public geofencesService: GeofencesService) {
     this.nombreZona = '';
     this.colorZona = '';
     this.visibleZona = '';
@@ -43,19 +42,49 @@ export class GeofencesModalComponent implements OnInit {
     this.velZona = '';
     this.tiempoActZona = '';
     this.geom = '';
-    this.updatedAt = new Date(); // O asigna un valor apropiado
+    this.updatedAt = new Date();
     this.descripcion = '';
-    this.bolEliminado = false; // O asigna un valor booleano apropiado
-  
+    this.bolEliminado = false;
     this.zoneType = '';
     this.geoTags = '';
-}
+  }
   guardarRegistro(): void {
 
     console.log('Datos recibidos:', this.nombreZona, this.colorZona);
     // Puedes realizar cualquier lógica adicional con los datos aquí
   }
+
+  fileKml(event: any) {
+    const file: File = event.target.files[0];
+    console.log("johan1", file);
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        const fileContent = e.target.result;
+        console.log("johan2", fileContent);
+        const regex = /<name>\s*([\s\S]*?)\s*<\/name>|<description>\s*([\s\S]*?)\s*<\/description>|<coordinates>\s*([\s\S]*?)\s*<\/coordinates>|<color>\s*([a-fA-F0-9]+)\s*<\/color>/g;
+        const datas: { name?: string, description?: string, coordinates?: string, color?: string }[] = [];
+        let item: { name?: string, description?: string, coordinates?: string, color?: string } = {};
+        let match;
+        while ((match = regex.exec(fileContent)) !== null) {
+          if (match[1] !== undefined) item['name'] = match[1].trim();
+          if (match[2] !== undefined) item['description'] = match[2].trim();
+          if (match[3] !== undefined) item['coordinates'] = match[3].trim();
+          if (match[4] !== undefined) {
+            item['color'] = match[4].trim();
+            datas.push(item);
+            item = {}; // Reiniciar el objeto para el próximo conjunto de coincidencias
+          }
+        }
+        console.log("johan3: ", datas);
+        this.datas.getProductsSmall().then(datas => this.datas = data);
+      };
+      reader.readAsText(file);
+    }
+  }
+  
   ngOnInit(): void {
+    
   }
 
 
